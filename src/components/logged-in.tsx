a9febd5913280dd0aa53e9d09035b5ef191729fc
n0rrman <email@henriknorrman.com>
2024-05-31 01:48:06 +0700

    build error fixed

--- src/components/logged-in.tsx
+++ src/components/logged-in.tsx
@@ -1,12 +1,25 @@
-export default async function LoggedIn() {
-  const res = await fetch(
-    "https://tuktukwan.henriknorrman.com/api/auth/status"
-  );
-  const body = await res.json();
+"use client";
 
-  if (body) {
+import { useEffect, useState } from "react";
+
+interface User {
+  id: string;
+}
+
+export default async function LoggedIn() {
+  const [user, setUser] = useState<User>({ id: "" });
+
+  useEffect(() => {
+    fetch("https://tuktukwan.henriknorrman.com/api/auth/status").then((res) =>
+      res.json().then((data) => {
+        setUser(data);
+      })
+    );
+  }, []);
+
+  if (user.id == "") {
     return <div>Not logged in</div>;
   } else {
-    return <div>{body.id} logged in</div>;
+    return <div>{user.id} logged in</div>;
   }
 }
