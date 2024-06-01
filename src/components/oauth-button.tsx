b32dafa4f3b9d3f2b0c3253f3e99e3e65a33c166
n0rrman <email@henriknorrman.com>
2024-06-02 00:36:01 +0700

    revalidate update

--- src/components/oauth-button.tsx
+++ src/components/oauth-button.tsx
@@ -1,3 +1,6 @@
+import { revalidatePath } from "next/cache";
+import Link from "next/link";
+
 interface OAuthButtonProps {
   name: string;
   icon: React.ReactElement;
@@ -14,13 +17,16 @@ export function OAuthButton({
   className,
 }: OAuthButtonProps) {
   return (
-    <a
+    <Link
+      onClick={() => {
+        revalidatePath("/");
+      }}
       href={`/api/auth/${name}`}
       className={`group hover:text-white flex flex-row gap-1.5 items-center text-lg border-2 py-5 px-7 rounded w-full ${className} ${borderColour} transition`}
     >
       <span className="text-5xl pr-4">{icon}</span>
       {register ? <span>Register using</span> : <span>Sign in with</span>}
       {name}
-    </a>
+    </Link>
   );
 }
