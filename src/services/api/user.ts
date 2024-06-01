4736da726082013e34dc75822e76f8d83354582b
n0rrman <email@henriknorrman.com>
2024-06-02 00:13:17 +0700

    env arg changed

--- src/services/api/user.ts
+++ src/services/api/user.ts
@@ -1,6 +1,6 @@
 export const getStatus = async () => {
     "use server";
-    return await fetch(`${process.env.HOST_URL}/api/auth/status`).then((res) => 
+    return await fetch(`${process.env.HOST_URL!}/api/auth/status`).then((res) => 
         res.json())
         // .then((data) => {
         //   setUser(data);
